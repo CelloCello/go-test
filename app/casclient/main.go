97b3fa956a641311e577ae0a3ea8002dcdecaf8e
CelloCello <cello1124@gmail.com>
2022-11-24 15:55:47 +0800

    feat: add cassandra test

---
+++ app/casclient/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gocql/gocql"
+)
+
+func main() {
+	cluster := gocql.NewCluster("127.0.0.1")
+	cluster.Keyspace = "mycasdb"
+	// cluster.Consistency = gocql.Quorum
+	// //设置连接池的数量,默认是2个（针对每一个host,都建立起NumConns个连接）
+	// cluster.NumConns = 3
+
+	session, _ := cluster.CreateSession()
+	time.Sleep(1 * time.Second) // Sleep so the fillPool can complete.
+	// fmt.Println(session.Pool.Size())
+	defer session.Close()
+
+	myMap :=     map[string]string{"first key": "first value", "second key": "second value", "third key": "third value", "fourth key": "fourth value", "fifth key": "fifth value"}
+	fmt.Print(myMap)
+
+	query :=  session.Query("SELECT * FROM user").Iter()
+	// if query != nil {
+	// 	log.Fatal(query)
+	// }
+
+	var id int
+	var name string
+	fmt.Println("=== LIST ===")
+	for query.Scan(&id, &name) {
+		fmt.Println("User:", id, name)
+	}
+}
